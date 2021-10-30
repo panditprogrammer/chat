5f96ae78a48e6747cf138f2945cf37dc4eb88ab5
panditprogrammer <panditprogrammer@gmail.com>
2021-10-30 12:24:50 +0530

    frontend and backend complete

--- js/client.js
+++ js/client.js
@@ -1,13 +1,54 @@
 const socket = io('http://localhost:8000');
 
+
 // html form 
-const form = document.getElementById("#form_container");
+const form = document.getElementById("form_container");
 // input box 
-const message_box = document.getElementById("#message_input");
+const message_box = document.getElementById("message_input");
 // show message container 
-const message_show_box = document.querySelector(".chat_body");
+const message_show_box = document.querySelector(".message_container");
+
+const add_html = (message, css_position)=>{
+    const message_show_span = document.createElement('span');
+    message_show_span.innerText = message;
+    message_show_span.classList.add(css_position);
+    message_show_box.append(message_show_span);
+}
+
+// send message 
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+
+    const message_value = message_box.value;
+    add_html(`${message_value}`,'right');
+    socket.emit('send',message_value);
+    message_box.value = "";
+})
+
+// name of user 
+let username = null;
+
+do
+{
+     username = prompt("Enter your name to join the live chat ");
+}while(username == null);
 
 // new user to join 
-const username = prompt("Enter your name to join the live chat ");
 console.log("user ",username)
 socket.emit('new-user-joined', username);
+
+socket.on('user-joined',username=>{
+    add_html(`${username} Joined the chat`, 'left');
+})
+
+// show message in chat_body 
+socket.on('receive', user_and_msg=>{
+    add_html(`${user_and_msg.username} - ${user_and_msg.message}`,'left');
+})
+
+// show message in chat_body 
+if(username !== null )
+socket.on('left', username=>{
+    add_html(`${username}  left...` ,'left');
+})
+
---
+++ node_server/index.js
@@ -0,0 +1,27 @@
+// This file will create node js server 
+
+
+const io = require('socket.io')(8000,{ // this is important 
+    cors:{origin:"*"}
+});
+
+const users = {};
+
+io.on('connection', socket =>{
+    socket.on('new-user-joined', username =>{
+        // console.log("New user is ", username);
+        users[socket.id] = username;
+        socket.broadcast.emit('user-joined', username);
+    });
+
+    socket.on('send', message =>{
+        socket.broadcast.emit('receive', {message: message, username: users[socket.id]});
+    });
+
+    socket.on('disconnect', message =>{
+        socket.broadcast.emit('left',  users[socket.id]);
+        delete users[socket.id];
+    });
+
+
+});
