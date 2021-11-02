966ce06b74756d5a8ee7c7e83ad675086e36e308
panditprogrammer <panditprogrammer@gmail.com>
2021-11-02 07:32:38 +0530

    including express.js

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 // This file will create node js server 
-
-
+const app = require('express')();
+const http = require('http').Server(app);
+// const io = require('socket.io')(http);
 const io = require('socket.io')(8080,{ // this is important 
     cors:{origin:"*"}
 });
+app.get('/', (req, res)=> {
+   res.send("index.html");
+});
+
+
+
 
 const users = {};
 
@@ -25,3 +32,7 @@ io.on('connection', socket =>{
 
 
 });
+
+http.listen(3000, function() {
+    console.log('listening on *:3000');
+ });
